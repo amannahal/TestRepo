b8eaace6705f942907a05eb87221260574719c64
amannahal <39898781+amannahal@users.noreply.github.com>
2018-06-04 15:30:11 +0530

    config.js
    Configuration file

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  fields: [
+    { label: "Due Date", id: "ghplus-due-date", type: "text", controlType: "datepicker" },
+    { label: "Hours Estimated", id: "ghplus-estimated-hours", type: "number" }
+  ],
+  repos: [
+    'amannahal/TestRepo'
+  ],
+  notInstalledMessage: "If you can see this, you do not have GitHub+ installed.  To install the extension, please (email this person/visit this url/wish really really hard)"
+};
